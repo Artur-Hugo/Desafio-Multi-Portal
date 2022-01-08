6fdd8611c74eb7c5f0f82ad2f4af2f0af72697fd
Artur-Hugo <Artur.Hugoliga@gmail.com>
2022-01-08 19:40:49 -0300

    Iniciando projeto

---
+++ src/main/java/com/DevOliga/DesafioMultiPortalApplication.java
@@ -0,0 +1,13 @@
+package com.DevOliga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMultiPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMultiPortalApplication.class, args);
+	}
+
+}
